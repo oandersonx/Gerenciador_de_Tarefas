0e599599d2fc3f2575e06c55fdf72080fb99c4b6
Anderson <anderson@gmail.com>
2019-10-07 03:38:58 -0300

    Configurando rota listar_tarefas

--- gerenciador_tarefas/app/views.py
+++ gerenciador_tarefas/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-def cadasatrar_tarefa():
-
+def listar_tarefas(request):
+    return render(request, 'tarefas/lista_tarefas.html')
 
 # Create your views here.
