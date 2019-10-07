fb17fe3319792bca31a71c29d1f77de27c2ef083
Anderson <anderson@gmail.com>
2019-10-07 03:51:35 -0300

    Passando tarefa (variavel) pelo método da view

--- gerenciador_tarefas/app/views.py
+++ gerenciador_tarefas/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 def listar_tarefas(request):
-    return render(request, 'tarefas/listar_tarefas.html')
+    nome_tarefa = "Passando essa tarefa pelo metodo da view"
+    return render(request, 'tarefas/listar_tarefas.html', {"nome_tarefa":nome_tarefa})
 
 # Create your views here.
