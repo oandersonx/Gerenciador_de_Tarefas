abc576c36605b34c3d88932b956a16848c42b2fb
Anderson <anderson@gmail.com>
2019-10-07 03:48:46 -0300

    bugs

--- gerenciador_tarefas/app/urls.py
+++ gerenciador_tarefas/app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('listar_tarefas/', listar_tarefas, name=listar_tarefas)
+    path('listar_tarefas/', listar_tarefas, name='listar_tarefas')
 ]
--- gerenciador_tarefas/app/views.py
+++ gerenciador_tarefas/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 def listar_tarefas(request):
-    return render(request, 'tarefas/lista_tarefas.html')
+    return render(request, 'tarefas/listar_tarefas.html')
 
 # Create your views here.
