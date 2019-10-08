16d72f561d21b53be48de6c70f75ddf81fe7563f
Anderson <anderson@gmail.com>
2019-10-08 01:55:56 -0300

    Diretorio 'entidades' e 'services'

---
+++ gerenciador_tarefas/app/services/tarefa_service.py
@@ -0,0 +1,5 @@
+from ..models import Tarefa
+
+def cadastrar_tarefa(tarefa):
+    Tarefa.objects.create(titulo=tarefa.titulo, descricao=tarefa.descricao,data_expiracao=tarefa.data_expiracao,
+                          prioridade=tarefa.prioridade)
--- gerenciador_tarefas/app/views.py
+++ gerenciador_tarefas/app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import TarefaForm
+from .entidades.tarefa import Tarefa
+from .services import tarefa_service
 
 def listar_tarefa(request):
     nome_tarefa = "Passando essa tarefa pelo metodo da view"
@@ -7,7 +9,18 @@ def listar_tarefa(request):
 
 
 def cadastar_tarefa(request):
-    form_tarefa = TarefaForm()
+    if request.method == "POST": #Se clicar no botao "Salvar"
+        form_tarefa = TarefaForm(request.POST)
+        if form_tarefa.is_valid(): #Se as informacoes digitadas estiverem OK
+            titulo = form_tarefa.cleaned_data["titulo"]
+            descricao= form_tarefa.cleaned_data["descricao"]
+            data_expiracao = form_tarefa.cleaned_data["data_expiracao"]
+            prioridade = form_tarefa.cleaned_data["prioridade"]
+            tarefa_nova = Tarefa(titulo=titulo, descricao=descricao,data_expiracao=data_expiracao,prioridade=prioridade)
+            tarefa_service.cadastrar_tarefa(tarefa_nova)
+            return redirect("listar_tarefa") #Volta para página "/listar_tarefa"
+    else:
+        form_tarefa = TarefaForm()
     return render(request, 'tarefas/form_tarefa.html',{"form_tarefa": form_tarefa})
 
 
